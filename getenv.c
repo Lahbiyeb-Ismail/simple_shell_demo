3932cb4ed3e7416805cb426a308467df3959f98c
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-12-02 17:21:19 +0100

    refactor and comment added to the _getenv function

--- getenv.c
+++ getenv.c
@@ -1,49 +1,56 @@
 #include "shell.h"
 
 /**
- * _getenv - Execute the command with its arguments
+ * _getenv - Get the value of an environment variable
  *
- * Description: This function creates a child process to execute
- * the command specified by the array of tokens using execve.
+ * Description: This function retrieves the value of an environment variable
+ * given its name. It searches through the environment variables until it finds
+ * a match for the specified name and returns the corresponding value. If the
+ * name is not found or if memory allocation fails during the process, the
+ * function returns NULL.
  *
- * @name: Array of tokens representing the command and its arguments.
+ * @name: The name of the environment variable to retrieve.
  *
- * Return: 0 on success, -1 on failure.
- *
-*/
-
+ * Return: A pointer to the value of the environment variable, or NULL if the
+ * variable is not found or if memory allocation fails.
+ */
 
 char *_getenv(char *name)
 {
 	char *env_cpy, *env_key, *env_val, *env;
-	int i = 0;
+	int i;
 
+	/* Check for NULL name or NULL environ */
 	if (name == NULL || environ == NULL)
 		return (NULL);
 
-	for (; environ[i]; i++)
+	for (i = 0; environ[i]; i++)
 	{
+		/* Duplicate the current environment variable for processing */
 		env_cpy = _strdup(environ[i]);
 		if (!env_cpy)
 			return (NULL);
 
+		/* Tokenize the environment variable to extract the key */
 		env_key = strtok(env_cpy, "=");
 
+		/* Check if the key matches the specified name */
 		if (_strcmp(env_key, name) == 0)
 		{
+			/* Tokenize to get the value part */
 			env_val = strtok(NULL, "\n");
 
-			if (env_val != NULL)
-			{
-				env = _strdup(env_val);
-				free(env_cpy);
-				return (env);
-			}
-			else
+			/* free the duplicated environment variable and return NULL */
+			if (!env_val)
 			{
 				free(env_cpy);
 				return (NULL);
 			}
+
+			/* Duplicate the value and free the duplicated environment variable */
+			env = _strdup(env_val);
+			free(env_cpy);
+			return (env);
 		}
 
 		free(env_cpy);
