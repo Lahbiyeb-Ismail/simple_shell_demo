f718eeb79a6d9fd8fb42a114c8cf9a868ec22e9a
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-12-03 17:58:46 +0100

    test the functionaliy with built_in 'setenv' function

--- hanlde_builtin_cmd.c
+++ hanlde_builtin_cmd.c
@@ -26,5 +26,6 @@ void handle_builtin_cmd(char **cmd, char **argv, int *exit_status, int cmd_idx)
 
 	/* Check if the command is the "env" built-in command */
 	else if (_strcmp(cmd[0], "setenv") == 0)
-		_setenv(cmd[1], cmd[2], 1);
+		/* TEST WITH THE BUILT-IN setenv FUNCTION*/
+		setenv(cmd[1], cmd[2], 1);
 }
