d5c322bff3feeb5a7b4e19be4c1863b9f663c37d
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-12-08 14:05:45 +0100

    test

--- main.c
+++ main.c
@@ -21,10 +21,8 @@ int main(int argc, char **argv)
 {
 	/* Buffer to store the user's input cmd */
 	char *cmd_line = NULL;
-	char *operator = NULL;
 	/* Array to store the tokens of the cmd */
 	char **cmd = NULL;
-	char **multi_cmd = NULL;
 
 	int exit_status = 0, cmd_idx = 0;
 	(void)argc;
@@ -43,46 +41,18 @@ int main(int argc, char **argv)
 		}
 		cmd_idx++;
 
-		/* TODO: FIX THE MEMORY ALLOCATION LEAK */
-		operator = check_for_operator(cmd_line);
+		/* Tokenize (split) the command and get the array of tokens */
+		cmd = tokenize_command(cmd_line, " \t\n");
 
-		if (operator)
-		{
-			int i;
+		if (!cmd)
+			continue;
 
-			multi_cmd = tokenize_command(cmd_line, operator);
-
-			for (i = 0; multi_cmd[i]; i++)
-			{
-					/* Tokenize (split) the command and get the array of tokens */
-				cmd = tokenize_command(multi_cmd[i], " \t\n");
-
-				if (!cmd)
-					continue;
-
-				if (check_if_builtin_cmd(cmd[0]))
-					exit_status = handle_builtin_cmd(cmd, argv, &exit_status, cmd_idx);
-				else
-				/* Execute the command and get the exit status */
-					exit_status = exec_command(cmd, argv, cmd_idx);
-			}
-		}
+		if (check_if_builtin_cmd(cmd[0]))
+			exit_status = handle_builtin_cmd(cmd, argv, &exit_status, cmd_idx);
 		else
-		{
-				/* Tokenize (split) the command and get the array of tokens */
-			cmd = tokenize_command(cmd_line, " \t\n");
-
-			if (!cmd)
-				continue;
-
-			if (check_if_builtin_cmd(cmd[0]))
-				exit_status = handle_builtin_cmd(cmd, argv, &exit_status, cmd_idx);
-			else
-			/* Execute the command and get the exit status */
-				exit_status = exec_command(cmd, argv, cmd_idx);
-		}
+		/* Execute the command and get the exit status */
+			exit_status = exec_command(cmd, argv, cmd_idx);
 	} while (1);
 
-	free(cmd_line);
 	free_memory(cmd);
 }
